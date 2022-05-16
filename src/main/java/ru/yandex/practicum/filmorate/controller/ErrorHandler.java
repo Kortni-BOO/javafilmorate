48b45b20143ca3b6ec14f67b23c7e2448feb745d
Kortni-BOO <58441596+Kortni-BOO@users.noreply.github.com>
2022-05-16 13:32:58 +0300

    Правки в ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -34,35 +34,3 @@ public class ErrorHandler {
     }
 
 }
-/*
-@ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIncorrectParameterException(final IncorrectParameterException e) {
-        return new ErrorResponse(
-                String.format("Ошибка с полем \"%s\".", e.getParameter())
-        );
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleInvalidEmailException(final InvalidEmailException e) {
-        return new ErrorResponse(
-                e.getMessage()
-        );
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handlePostNotFoundException(final PostNotFoundException e) {
-        return new ErrorResponse(
-                e.getMessage()
-        );
-    }
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handlePostNotFoundException(final UserAlreadyExistException e) {
-        return new ErrorResponse(
-                e.getMessage()
-        );
-    }
-*/
